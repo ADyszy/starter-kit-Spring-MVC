47019ea435e0b3297cc837fff13e493ae77e659b
ADyszy <ADYSZY@CE11704.corp.capgemini.com>
2015-08-07 16:15:58 +0200

    css fun

--- web/src/main/java/pl/spring/demo/web/rest/BookRestService.java
+++ web/src/main/java/pl/spring/demo/web/rest/BookRestService.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.*;
 
 import pl.spring.demo.service.BookService;
 import pl.spring.demo.service.UnknownParameterException;
+import pl.spring.demo.service.impl.NotExistingBookObjectUpdateException;
 import pl.spring.demo.to.BookTo;
 
 import java.util.List;
@@ -21,8 +22,8 @@ public class BookRestService {
 	public List<BookTo> findBooksByTitle(@RequestParam("titlePrefix") String titlePrefix) {
 		return bookService.findBooksByTitle(titlePrefix);
 	}
-	
-	@RequestMapping(value = "/book", method = RequestMethod.DELETE) 
+
+	@RequestMapping(value = "/book", method = RequestMethod.DELETE)
 	public void book(@RequestParam("id") Long bookId) {
 		bookService.deleteBook(bookId);
 	}
@@ -31,11 +32,11 @@ public class BookRestService {
 	public BookTo saveBook(@RequestBody BookTo book) {
 		return bookService.saveBook(book);
 	}
-	
+
 	@RequestMapping(value = "/update-book", method = RequestMethod.PUT)
-	public void book(	@RequestParam("id") Long bookId, 
-			@RequestParam("paramName") String paramName,
-			@RequestParam("value") Object value) throws UnknownParameterException {
+	public void book(@RequestParam("id") Long bookId, @RequestParam("paramName") String paramName,
+			@RequestParam("value") Object value)
+					throws UnknownParameterException, NotExistingBookObjectUpdateException {
 		bookService.updateBook(bookId, paramName, value);
 	}
 }
